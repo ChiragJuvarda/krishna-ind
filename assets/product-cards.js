847c43f2e53ac66947e6186d4d949c51801533fa
ChiragJuvarda <115066947+ChiragJuvarda@users.noreply.github.com>
2026-02-11 12:25:26 +0530

    Implement Product Cards section with Swiper integration and sample slides

--- assets/product-cards.js
+++ assets/product-cards.js
@@ -3,3 +3,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   // user will paste JS here
 });
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
